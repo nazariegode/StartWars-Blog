cde0ea957da920b632c7e032be4d47ac941165ba
David Nazariego Ulacio <dnazariego@gmail.com>
2024-07-08 21:21:09 +0000

    avance1

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,42 +1,96 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			url: 'https://playground.4geeks.com',
+			agenda: 'nazariego',
+			contacts: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			getAgenda: () => {
+				const { url, agenda } = getStore()
+				const { createAgenda } = getActions()
+				fetch(`${url}/contact/agendas/${agenda}`)
+					.then((response) => {
+						console.log(response)
+						if (response.status === 404) createAgenda()
+						else return response.json()
+					})
+					.then((responseJson) => {
+						console.log(responseJson)
+						setStore({ contacts: responseJson.contacts })
+					})
 
-				//reset the global store
-				setStore({ demo: demo });
+			},
+
+			createAgenda: () => {
+				const { url, agenda } = getStore()
+				const { getAgenda } = getActions()
+				fetch(`${url}/contact/agendas/${agenda}`, {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' }
+				})
+					.then((response) => {
+						console.log(response)
+						if (response.status === 201) getAgenda()
+						return response.json()
+					})
+					.then((responseJson) => {
+						console.log(responseJson)
+					})
+			},
+
+			createContact: (datos) => {
+				const { url, agenda } = getStore()
+				const { getAgenda } = getActions()
+				console.log(datos)
+				fetch(`${url}/contact/agendas/${agenda}/contacts`, {
+					method: 'POST',
+					body: JSON.stringify(datos),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+					.then((response) => {
+						if (response.status === 201) getAgenda()
+						return response.json()
+					})
+					.then((datos) => console.log(datos))
+			},
+
+			updateContact: (datos, id) => {
+				const { url, agenda } = getStore()
+				const { getAgenda } = getActions()
+				console.log(datos)
+				fetch(`${url}/contact/agendas/${agenda}/contacts/${id}`, {
+					method: 'PUT',
+					body: JSON.stringify(datos),
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+					.then((response) => {
+						if (response.status === 200) getAgenda()
+						return response.json()
+					})
+					.then((datos) => console.log(datos))
+			},
+
+			deleteContact: (id) => {
+				const { url, agenda } = getStore()
+				const { getAgenda } = getActions()
+				fetch(`${url}/contact/agendas/${agenda}/contacts/${id}`, {
+					method: 'DELETE',
+					headers: {
+						'Content-Type': 'application/json'
+					}
+				})
+					.then((response) => {
+						console.log(response)
+						if (response.status === 204) getAgenda()
+						return response.json()
+					})
+					.then((datos) => console.log(datos))
 			}
 		}
 	};
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,40 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useContext, useState } from "react";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
+import Card from "../component/card"
+import Modal from "../component/modal"
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [show, setShow] = useState(false)
+	const [contacto, setContacto] = useState(null)
+	const { store, actions } = useContext(Context)
+
+	return (
+		<>
+			<div className="container">
+				<div className="row">
+					<div className="col-md-12 text-white">
+						<h2>Contact List</h2>
+						<h6>Agenda asignada: {store.agenda}</h6>
+					</div>
+					<div className="col-md-10">
+						{
+							store.contacts.length > 0 &&
+							store.contacts.map((contact) => {
+								return (
+									<Card {...contact} key={contact.id} onClick={() => {
+										setContacto(contact)
+										setShow(true)
+										//const consultar = confirm(`Desea eliminar el contacto: ${contact.name}?`)
+										//if (consultar) actions.deleteContact(contact.id)
+									}} />
+								)
+							})
+						}
+					</div>
+				</div>
+			</div>
+			<Modal show={show} setShow={setShow} contacto={contacto} setContacto={setContacto} deleteContact={actions.deleteContact} />
+		</>
+	)
+};
